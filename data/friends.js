9ba089a8167357c8f18a267c85c305764ac9d069
yzou235 <yumingzou235@gmail.com>
2024-01-17 18:50:02 -0500

    Added dummy data

---
+++ data/friends.js
@@ -0,0 +1,32 @@
+const friends = [
+    {
+        id: 1,
+        name: 'Harry Potter',
+        email: 'h.potter@hogwarts.edu',
+        phone: '1234567890',
+        image: "./images/hp.jpg"
+    },
+    {
+        id: 2,
+        name: 'Ron Weasley',
+        email: 'r.weasley@hogwarts.edu',
+        phone: '2234567890',
+        image: "./images/rw.jpg"
+    },
+    {
+        id: 3,
+        name: 'Hermione Granger',
+        email: 'h.granger@hogwarts.edu',
+        phone: '3234567890',
+        image: "./images/hg.jpg"
+    },
+    {
+        id: 4,
+        name: 'Dobby',
+        email: 'dobby@hogwarts.edu',
+        phone: '4234567890',
+        image: "./images/dobby.jpg"
+    },
+]
+
+module.exports = friends;
---
+++ data/posts.js
@@ -0,0 +1,84 @@
+const posts = [
+    {
+        id:1,
+        friendId:1,
+        friendName: 'Harry Potter',
+        date: "2004-10-10",
+        event:"Quidditch Together",
+        content:"Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui."
+    },
+    {
+        id:2,
+        friendId:4,
+        friendName: 'Dobby',
+        date: "2004-10-10",
+        event:"Tea Time",
+        content:"Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi."
+    },
+    {
+        id:3,
+        friendId:3,
+        friendName: 'Hermione Granger',
+        date: "2004-10-10",
+        event:"History of Magic Study Hall",
+        content:"Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam."
+    },
+    {
+        id:4,
+        friendId:2,
+        friendName: 'Ron Weasley',
+        date: "2004-10-10",
+        event:"visit the Weasley family",
+        content:"Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum."
+    },
+    {
+        id:5,
+        friendId:4,
+        friendName: 'Dobby',
+        date: "2004-10-10",
+        event:"try Hogwarts' new dinner menu",
+        content:"Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero."
+    },
+    {
+        id:6,
+        friendId:3,
+        friendName: 'Hermione Granger',
+        date: "2004-10-10",
+        event:"Bookstore!",
+        content:"In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet."
+    },
+    {
+        id:7,
+        friendId:1,
+        friendName: 'Harry Potter',
+        date: "2004-10-10",
+        event:"Movie Night",
+        content:"Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat."
+    },
+    {
+        id:8,
+        friendId:1,
+        friendName: 'Harry Potter',
+        date: "2004-10-10",
+        event:"Lunch Together",
+        content:"In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi."
+    },
+    {
+        id:9,
+        friendId:2,
+        friendName: 'Ron Weasley',
+        date: "2004-10-10",
+        event:"visit the Burrow",
+        content:"Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis."
+    },
+    {
+        id:10,
+        friendId:4,
+        friendName: 'Dobby',
+        date: "2004-10-10",
+        event:"Plaisir, Le",
+        content:"Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus."
+    }
+];
+
+module.exports = posts;
